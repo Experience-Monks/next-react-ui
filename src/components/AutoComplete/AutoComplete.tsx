aac2c2ddf3fffa8eb01aeac2af54739943673406
Felix Zhang <felix.zhang@jam3.com>
2022-06-21 09:01:22 -0400

    fix: fixed Autocomplete import issue

--- src/components/AutoComplete/AutoComplete.tsx
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -1,9 +1,8 @@
 import { memo, useCallback, useEffect, useState } from 'react';
 import classnames from 'classnames';
+import typingAhead from 'typing-ahead';
 
 import styles from './AutoComplete.module.scss';
-
-const typingAhead = require('typing-ahead');
 
 export type Props = {
   className?: string;
@@ -12,20 +11,25 @@ export type Props = {
   autoCompleteResult: Function;
 };
 
+type TypingAheadOption = {
+  generate: (param: Array<string>) => string;
+  find: (term: string, model: string) => Array<string>;
+};
+
 const AutoComplete = ({ id, data, className, autoCompleteResult }: Props) => {
-  const [model, setModel] = useState(null);
+  const [model, setModel] = useState<string>('');
 
   const onInputChange = useCallback(
     ({ target: { value } }) => {
       if (model) {
-        autoCompleteResult(value ? typingAhead.find(value, model) : null);
+        autoCompleteResult(value ? (typingAhead as TypingAheadOption).find(value, model) : null);
       }
     },
     [autoCompleteResult, model]
   );
 
   useEffect(() => {
-    setModel(typingAhead.generate(data));
+    setModel((typingAhead as TypingAheadOption).generate(data));
   }, [data]);
 
   return <input id={id} className={classnames(styles.AutoComplete, className)} onChange={onInputChange} />;
